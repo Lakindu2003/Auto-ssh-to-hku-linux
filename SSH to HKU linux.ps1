de6eccd118db1d4d3a128f9fc1d91d379cc4a5ae
Lakindu2003 <lakindujay@gmail.com>
2023-02-06 23:43:32 +0800

    Update SSH to HKU linux.ps1

--- SSH to HKU linux.ps1
+++ SSH to HKU linux.ps1
@@ -1,2 +1,4 @@
 Set-Location '~\OneDrive\Desktop\some random projects' | python3 '.\Login to hku linux.py'
 # replace '~\OneDrive\Desktop\some random projects' with the file path to your .py file
+# should run   [Set-ExecutionPolicy RemoteSigned] on command prompt as powershell files are disabled by default
+# read for more info on running powershell scripts https://shellgeek.com/powershell-fix-running-scripts-is-disabled-on-this-system/#:~:text=While%20running%20PowerShell%20script%2C%20if,t%20allow%20to%20run%20script.&text=PowerShell%20has%20built%2Din%20security%20features%20implemented.
