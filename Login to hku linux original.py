26e57e45a67e01859cee1a351f5f1c57220b6a7d
Lakindu2003 <lakindujay@gmail.com>
2023-02-06 23:07:43 +0800

    Add files via upload

---
+++ Login to hku linux original.py
@@ -0,0 +1,21 @@
+import os
+from pyautogui import write, press
+from time import sleep 
+
+#make sure to instal pyautogui by running the below command on the terminal
+#pip install pyautogui 
+def x():
+    press("enter")
+
+os.startfile(r"") #paste file location of command prompt. don't remove the 'r'.
+sleep(1)
+write("ssh @gatekeeper.cs.hku.hk") #enter cs account
+x()
+sleep(1)
+write("") #enter password
+x()
+write("ssh @academy.cs.hku.hk") #enter cs account
+x()
+sleep(2)
+write("") #enter password
+x()
